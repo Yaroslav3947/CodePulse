d5aacbc1e5a085d8bdb62049cf58e54d27649330
Yaroslav <kogutyaroslav3947@gmail.com>
2023-12-02 17:08:24 -0500

    added POST service & postSubmission

--- UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { AddBlogPost } from '../Models/add-blog-post.model';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { BlogPostService } from '../services/blog-post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-blogpost',
@@ -9,7 +11,7 @@ import { AddBlogPost } from '../Models/add-blog-post.model';
 export class AddBlogpostComponent {
   model: AddBlogPost;
 
-  constructor() {
+  constructor(private blogPostService: BlogPostService, private router: Router) {
     this.model = {
       title: '',
       shortDescription: '',
@@ -23,7 +25,12 @@ export class AddBlogpostComponent {
   }
 
   onFormSubmit(): void {
-    console.log(this.model);
+    this.blogPostService.createBlogPost(this.model)
+    .subscribe({
+      next: (response) => {
+        this.router.navigateByUrl('/admin/blogposts');
+      }
+    });
   }
 
 }
---
+++ UI/codepulse/src/app/features/blog-post/services/blog-post.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostService {
+
+  constructor(private http: HttpClient) { }
+
+  createBlogPost(data: AddBlogPost): Observable<BlogPost> {
+    return this.http.post<BlogPost>(`${environment.apiBaseUrl}/api/blogposts`, data);
+  }
+}
