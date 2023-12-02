3db0a3f8249d6a839b9ea313e493b554e7a5a45b
Yaroslav <kogutyaroslav3947@gmail.com>
2023-12-02 16:01:02 -0500

    added UI for addBlogPost

---
+++ UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-blogpost',
+  templateUrl: './add-blogpost.component.html',
+  styleUrls: ['./add-blogpost.component.css']
+})
+export class AddBlogpostComponent {
+
+}
