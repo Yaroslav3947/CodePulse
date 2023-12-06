8a2cf8fb666fdfb762a0557b0ffdae7ba2620106
Yaroslav <kogutyaroslav3947@gmail.com>
2023-12-06 08:11:07 -0500

    added dropdown list for categories(UI)

--- UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../services/blog-post.service';
 import { Router } from '@angular/router';
+import { CategoryService } from '../../category/services/category.service';
+import { Observable } from 'rxjs'
+import { Category } from '../../category/models/category.model';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent {
+export class AddBlogpostComponent implements OnInit {
   model: AddBlogPost;
+  categories$?: Observable<Category[]>;
 
-  constructor(private blogPostService: BlogPostService, private router: Router) {
+  constructor(private blogPostService: BlogPostService, private router: Router, private categoryService: CategoryService) {
     this.model = {
       title: '',
       shortDescription: '',
@@ -31,6 +35,10 @@ export class AddBlogpostComponent {
         this.router.navigateByUrl('/admin/blogposts');
       }
     });
+  }
+
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
   }
 
 }
