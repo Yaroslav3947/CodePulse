aad5ab6ae22b80d1f9c3ffc58b3f81b7cc389289
Yaroslav <kogutyaroslav3947@gmail.com>
2023-12-09 18:02:50 -0500

    added BlogImages to Db

---
+++ API/CodePulse.API/CodePulse.API/Models/Domain/BlogImage.cs
@@ -0,0 +1,10 @@
+﻿namespace CodePulse.API.Models.Domain {
+    public class BlogImage {
+        public Guid Id { get; set; }
+        public string FileName { get; set; }
+        public string FileExtention { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
