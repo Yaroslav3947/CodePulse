b6100a93b50348e20d2d7cf9db22e0ebdc9ddd86
Yaroslav <kogutyaroslav3947@gmail.com>
2023-12-11 17:41:32 -0500

    added Image Service to upload images + debug Db

---
+++ API/CodePulse.API/CodePulse.API/Migrations/20231211223528_Adding BlogImage Domain Model1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodePulse.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingBlogImageDomainModel1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FileExtention",
+                table: "BlogImages",
+                newName: "FileExtension");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FileExtension",
+                table: "BlogImages",
+                newName: "FileExtention");
+        }
+    }
+}
