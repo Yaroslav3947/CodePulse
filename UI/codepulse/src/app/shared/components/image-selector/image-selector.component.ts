eb4b3c55d1d281a9fa207f1eea2f23b8a233d397
Yaroslav <kogutyaroslav3947@gmail.com>
2023-12-14 17:35:46 -0500

    added images

--- UI/codepulse/src/app/shared/components/image-selector/image-selector.component.ts
+++ UI/codepulse/src/app/shared/components/image-selector/image-selector.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ImageService } from './image.service';
+import { Observable } from 'rxjs';
+import { BlogImage } from '../../models/blog-image.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-image-selector',
   templateUrl: './image-selector.component.html',
   styleUrls: ['./image-selector.component.css']
 })
-export class ImageSelectorComponent {
+export class ImageSelectorComponent implements OnInit {
   private file?: File;
   fileName: string = '';
   title: string = '';
+  @ViewChild('form', {static: false}) imageUploadForm?: NgForm;
+  images$?: Observable<BlogImage[]>;
 
   constructor(private imageService: ImageService) {
     
+  }
+
+  ngOnInit(): void {
+    this.getImages();
   }
 
 
@@ -26,10 +35,15 @@ export class ImageSelectorComponent {
       this.imageService.uploadImage(this.file, this.fileName, this.title)
       .subscribe({
         next: (response) => {
-          console.log(response);
+          this.imageUploadForm?.resetForm();
+          this.getImages();
         }
       });
     }
+  }
+
+  private getImages() {
+    this.images$ = this.imageService.getAllImages();
   }
   
 }
