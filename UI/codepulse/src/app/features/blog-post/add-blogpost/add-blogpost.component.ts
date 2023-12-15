54d23f76ebf4eb8d9cb6e6f63eb017a415d2a030
Yaroslav <kogutyaroslav3947@gmail.com>
2023-12-15 16:42:36 -0500

    implemented ImageSelector in add-blogpost form

--- UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../services/blog-post.service';
 import { Router } from '@angular/router';
 import { CategoryService } from '../../category/services/category.service';
-import { Observable } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
 import { Category } from '../../category/models/category.model';
+import { ImageService } from 'src/app/shared/components/image-selector/image.service';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent implements OnInit {
+export class AddBlogpostComponent implements OnInit, OnDestroy {
   model: AddBlogPost;
   categories$?: Observable<Category[]>;
 
-  constructor(private blogPostService: BlogPostService, private router: Router, private categoryService: CategoryService) {
+  isImageSelectorVisible: boolean = false;
+
+  imageSelectorSubscription?: Subscription;
+
+
+  constructor(private blogPostService: BlogPostService,
+    private router: Router,
+    private categoryService: CategoryService,
+    private imageService: ImageService) {
     this.model = {
       title: '',
       shortDescription: '',
@@ -32,16 +41,39 @@ export class AddBlogpostComponent implements OnInit {
   onFormSubmit(): void {
     console.log(this.model);
     this.blogPostService.createBlogPost(this.model)
-    .subscribe({
-      next: (response) => {
-        this.router.navigateByUrl('/admin/blogposts');
-      }
-    });
+      .subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/blogposts');
+        }
+      });
   }
-  
+
 
   ngOnInit(): void {
     this.categories$ = this.categoryService.getAllCategories();
+
+    this.imageSelectorSubscription = this.imageService.onSelectImage()
+      .subscribe({
+        next: (response) => {
+          if (this.model) {
+            this.model.featuredImageUrl = response.url;
+            this.isImageSelectorVisible = false;
+          }
+        }
+      })
   }
+
+  openImageSelector(): void {
+    this.isImageSelectorVisible = true;
+  }
+
+  closeImageSelector(): void {
+    this.isImageSelectorVisible = false;
+  }
+
+  ngOnDestroy(): void {
+    this.imageSelectorSubscription?.unsubscribe();
+  }
+
 
 }
