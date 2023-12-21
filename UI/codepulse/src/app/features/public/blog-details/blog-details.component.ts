3e39825c46291b79c9a067e13d00e3b911e14556
Yaroslav <kogutyaroslav3947@gmail.com>
2023-12-21 03:51:18 -0500

    implemented UI for the blog-details

--- UI/codepulse/src/app/features/public/blog-details/blog-details.component.ts
+++ UI/codepulse/src/app/features/public/blog-details/blog-details.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BlogPostService } from '../../blog-post/services/blog-post.service';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../../blog-post/models/blog-post.model';
 
 @Component({
   selector: 'app-blog-details',
-  standalone: true,
-  imports: [],
+  // standalone: true,
+  // imports: [],
   templateUrl: './blog-details.component.html',
-  styleUrl: './blog-details.component.css'
+  styleUrls: ['./blog-details.component.css']
 })
 export class BlogDetailsComponent implements OnInit {
 
   url: string | null = null;
-    constructor(private route: ActivatedRoute) {
+  blogPost$?: Observable<BlogPost>;
+
+    constructor(private route: ActivatedRoute,
+      private blogPostService: BlogPostService) {
 
     }
   ngOnInit(): void {
@@ -23,5 +29,9 @@ export class BlogDetailsComponent implements OnInit {
     });
 
     // Fetch blog details by url
+    if(this.url) {
+      this.blogPost$ = this.blogPostService.getBlogPostByUrlHandle(this.url)
+    }
+
   }
 }
