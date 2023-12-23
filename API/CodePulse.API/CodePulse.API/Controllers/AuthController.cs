d2abcf6b64b48f0daafb7f87f2746f175647117c
Yaroslav <kogutyaroslav3947@gmail.com>
2023-12-22 21:03:31 -0500

    Created Identity Tables

--- API/CodePulse.API/CodePulse.API/Controllers/AuthController.cs
+++ API/CodePulse.API/CodePulse.API/Controllers/AuthController.cs
@@ -14,6 +14,8 @@ namespace CodePulse.API.Controllers {
             this._userManager = userManager;
         }
 
+        public 
+
         // POST: {apibaseurl}/api/auth/register
         [HttpPost]
         [Route("register")]
