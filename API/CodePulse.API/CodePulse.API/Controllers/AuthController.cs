d70158a3037c96b029cb2763c7d6d1b7c0aa9cfc
Yaroslav <kogutyaroslav3947@gmail.com>
2023-12-22 20:35:07 -0500

    added Auth controller and Register method

---
+++ API/CodePulse.API/CodePulse.API/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+﻿using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        // POST: {apibaseurl}/api/auth/register
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto request) {
+            // Create IdentityUser object
+
+            var user = new IdentityUser {
+                UserName = request.Email?.Trim(),
+                Email = request.Email?.Trim()
+            };
+
+            // Create User
+            var identityResult = await _userManager.CreateAsync(user, request.Password);
+
+            if(identityResult.Succeeded) {
+                // Add Role to user(Reader)
+                var identiryResult = await _userManager.AddToRoleAsync(user, "Reader");
+                
+                if(identiryResult.Succeeded) {
+                    return Ok();
+                } else {
+                    if(identityResult.Errors.Any()) {
+                        foreach(var error in identityResult.Errors) {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+            } else {
+                if(identityResult.Errors.Any()) {
+                    foreach (var error in identityResult.Errors) {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return ValidationProblem(ModelState);
+        }
+    }
+}
