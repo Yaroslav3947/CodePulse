d259bdb5af4929bddc6a1f0bfbb613f8b1713594
Yaroslav <kogutyaroslav3947@gmail.com>
2024-01-30 05:14:02 -0500

    added LikeContext

---
+++ API/CodePulse.API/CodePulse.API/Models/Domain/BlogPostLike.cs
@@ -0,0 +1,7 @@
+﻿namespace CodePulse.API.Models.Domain {
+    public class BlogPostLike {
+        public Guid Id { get; set; }
+        public Guid BlogPostId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
