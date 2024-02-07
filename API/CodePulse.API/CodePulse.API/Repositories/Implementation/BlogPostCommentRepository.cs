9c9f853bb63eb8a558aa4ce961b786a014f673af
Yaroslav <kogutyaroslav3947@gmail.com>
2024-02-07 16:30:14 -0500

    implemented GetBlogPostCommentsByIdAsync

---
+++ API/CodePulse.API/CodePulse.API/Repositories/Implementation/BlogPostCommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Repositories.Implementation {
+    public class BlogPostCommentRepository : IBlogPostCommentRepository {
+        private readonly ApplicationDbContext _dbContext;
+
+        public BlogPostCommentRepository(ApplicationDbContext dbContext) {
+            this._dbContext = dbContext;
+        }
+        public async Task<IEnumerable<BlogPostComment>> GetBlogPostCommentsByIdAsync(Guid blogPostId) {
+            return await _dbContext.BlogPostComments.
+                Where(x => x.BlogPostId == blogPostId).ToListAsync();
+        }
+    }
+}
---
+++ API/CodePulse.API/CodePulse.API/Repositories/Interface/IBlogPostCommentRepository.cs
@@ -0,0 +1,7 @@
+﻿using CodePulse.API.Models.Domain;
+
+namespace CodePulse.API.Repositories.Interface {
+    public interface IBlogPostCommentRepository {
+        Task<IEnumerable<BlogPostComment>> GetBlogPostCommentsByIdAsync(Guid blogPostId);
+    }
+}
