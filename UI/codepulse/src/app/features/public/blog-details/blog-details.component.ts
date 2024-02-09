120eef2d334a9bf23ad493f1c252392579ec1a57
Yaroslav <kogutyaroslav3947@gmail.com>
2024-02-09 16:55:34 -0500

    rm rubbish

--- UI/codepulse/src/app/features/public/blog-details/blog-details.component.ts
+++ UI/codepulse/src/app/features/public/blog-details/blog-details.component.ts
@@ -99,23 +99,4 @@ export class BlogDetailsComponent implements OnInit {
     }
   }
 
-  getCommentDateTimeAgo(commentDate: Date): string {
-    const currentDate = new Date();
-  const timeDifference = currentDate.getTime() - commentDate.getTime();
-
-  const seconds = Math.floor(timeDifference / 1000);
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-  const days = Math.floor(hours / 24);
-
-  if (days > 0) {
-    return `${days} day${days > 1 ? 's' : ''} ago`;
-  } else if (hours > 0) {
-    return `${hours} hour${hours > 1 ? 's' : ''} ago`;
-  } else if (minutes > 0) {
-    return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
-  } else {
-    return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
-  }
-  }
 }
