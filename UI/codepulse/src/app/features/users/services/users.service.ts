1f17af619caee74e8bb0249ef51aebcf431a14b2
Yaroslav <kogutyaroslav3947@gmail.com>
2024-02-23 10:23:45 -0500

    added service for update user

--- UI/codepulse/src/app/features/users/services/users.service.ts
+++ UI/codepulse/src/app/features/users/services/users.service.ts
@@ -4,6 +4,7 @@ import { CookieService } from 'ngx-cookie-service';
 import { environment } from 'src/environments/environment.development';
 import { User } from '../models/user.model';
 import { Observable } from 'rxjs';
+import { UpdateUserRequest } from '../models/update-user-request';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,10 @@ export class UsersService {
 
   getAllUsers(): Observable<User[]> {
     return this.http.get<User[]>(`${environment.apiBaseUrl}/api/users?addAuth=true`);
+  }
+
+  updateUser(id: string, updateUserRequest: UpdateUserRequest): Observable<User> {
+    return this.http.put<User>(`${environment.apiBaseUrl}/api/users/${id}?addAuth=true`, 
+    updateUserRequest);
   }
 }
