dc0b0ba5fa882a001cd4e29e6e6a95523211ccb2
Yaroslav <kogutyaroslav3947@gmail.com>
2024-04-28 21:59:17 +0200

    implemented searcher

--- UI/codepulse/src/app/features/public/home/home.component.ts
+++ UI/codepulse/src/app/features/public/home/home.component.ts
@@ -5,8 +5,6 @@ import { BlogPost } from '../../blog-post/models/blog-post.model';
 
 @Component({
   selector: 'app-home',
-  // standalone: true,
-  // imports: [],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -14,14 +12,36 @@ import { BlogPost } from '../../blog-post/models/blog-post.model';
 
 export class HomeComponent implements OnInit {
 
-  blogs$?: Observable<BlogPost[]>;
-  constructor(private blogPostService: BlogPostService) {
+  filteredBlogs: BlogPost[] = [];
 
+  blogposts: BlogPost[] = [];
+
+  searchName: string = '';
+
+  blogs$?: Observable<BlogPost[]>;
+
+  constructor(private blogPostService: BlogPostService) {
+    this.blogs$ = this.blogPostService.getAllBlogPosts();
+    this.blogPostService.getAllBlogPosts().subscribe(
+     blogs => {
+     this.blogposts = blogs;
+     this.filteredBlogs = blogs;
+   });
+  }
+
+  filterByName() {
+    console.log(this.filteredBlogs.length)
+    if (this.searchName.trim() === '') {
+      this.filteredBlogs = this.blogposts;
+    } else {
+      this.filteredBlogs = this.blogposts.filter(blogpost =>
+        blogpost.title.toLowerCase().includes(this.searchName.toLowerCase())
+      );
+    }
   }
 
   ngOnInit():void {
      this.blogs$ = this.blogPostService.getAllBlogPosts();
   }
-
 
 }
